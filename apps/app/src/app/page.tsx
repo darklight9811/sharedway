49a1310e0892c0b7c4e62e0351c833ab9cf62d63
Rafael Correa <rafael.chaves98@hotmail.com>
2024-03-17 01:35:37 -0300

    start preparing app to receive real content

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -1,25 +1,8 @@
 import { Button } from "@repo/ds/ui/button";
-import { Card } from "@repo/ds/ui/card";
-
-const LINKS = [
-  {
-    title: "Docs",
-    href: "https://turbo.build/repo/docs",
-    description: "Find in-depth information about Turborepo features and API.",
-  },
-];
 
 export default function Page(): JSX.Element {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <div className="grid mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        {LINKS.map(({ title, href, description }) => (
-          <Card href={href} key={title} title={title}>
-            {description}
-          </Card>
-        ))}
-      </div>
-
+    <main className="">
 	  <Button>Teste</Button>
     </main>
   );
