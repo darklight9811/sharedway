893495a95469ae8eef73242616cf287b96bed3da
Rafael Correa <rafael.chaves98@hotmail.com>
2024-03-17 01:27:28 -0300

    fix missing update links

--- apps/app/tailwind.config.ts
+++ apps/app/tailwind.config.ts
@@ -1,10 +1,10 @@
 // tailwind config is required for editor support
 
 import type { Config } from "tailwindcss";
-import sharedConfig from "../../packages/ui/tailwind.config";
+import sharedConfig from "../../packages/ds/tailwind.config";
 
 const config: Pick<Config, "content" | "presets"> = {
-  content: ["./src/app/**/*.tsx", "../../packages/ui/src/**/*.tsx"],
+  content: ["./src/app/**/*.tsx", "../../packages/ds/src/**/*.tsx"],
   presets: [sharedConfig],
 };
 
