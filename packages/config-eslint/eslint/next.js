94897013de91dca5297befc650480ed721a8884e
Rafael Correa <rafael.chaves98@hotmail.com>
2024-03-16 21:45:51 -0300

    fix ui library

--- packages/config-eslint/eslint/next.js
+++ packages/config-eslint/eslint/next.js
@@ -3,8 +3,8 @@ const { resolve } = require("node:path");
 const project = resolve(process.cwd(), "tsconfig.json");
 
 /*
- * This is a custom ESLint configuration for use a library
- * that utilizes React.
+ * This is a custom ESLint configuration for use with
+ * Next.js apps.
  *
  * This config extends the Vercel Engineering Style Guide.
  * For more information, see https://github.com/vercel/style-guide
@@ -13,14 +13,18 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 module.exports = {
   extends: [
-    "@vercel/style-guide/eslint/browser",
+    "@vercel/style-guide/eslint/node",
     "@vercel/style-guide/eslint/typescript",
+    "@vercel/style-guide/eslint/browser",
     "@vercel/style-guide/eslint/react",
+    "@vercel/style-guide/eslint/next",
+    "eslint-config-turbo",
   ].map(require.resolve),
   parserOptions: {
     project,
   },
   globals: {
+    React: true,
     JSX: true,
   },
   settings: {
@@ -33,9 +37,10 @@ module.exports = {
       },
     },
   },
-  ignorePatterns: ["node_modules/", "dist/", ".eslintrc.js", "**/*.css"],
+  ignorePatterns: ["node_modules/", "dist/"],
   // add rules configurations here
   rules: {
     "import/no-default-export": "off",
+	"no-html-link-for-pages": "off"
   },
 };
