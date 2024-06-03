f52630e38fb6aab138081e7e894e3ec63a56ffe4
Rafael Correa <rafael.chaves98@hotmail.com>
2024-06-03 01:10:17 -0300

    fix: issues with eslintrc

--- apps/app/.eslintrc.js
+++ apps/app/.eslintrc.js
@@ -1,3 +1,17 @@
+const { resolve } = require("node:path")
+
+const project = resolve(process.cwd(), "apps/app/tsconfig.json")
+
 module.exports = {
-  extends: ["../../packages/config/eslint/next.js"],
-};
+	extends: ["../../packages/config/eslint/next.js"],
+	parserOptions: {
+		project,
+	},
+	settings: {
+		"import/resolver": {
+			typescript: {
+				project,
+			},
+		},
+	},
+}
