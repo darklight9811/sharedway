868bdafda30fa9d00020eb8553f0ac00dfae32f3
Rafael Correa <rafael.chaves98@hotmail.com>
2024-07-09 20:11:20 -0300

    feat: added translations to create page

--- apps/app/src/app/[locale]/(app)/entities/new/page.tsx
+++ apps/app/src/app/[locale]/(app)/entities/new/page.tsx
@@ -2,20 +2,23 @@ import { Link } from "@/lib/navigation";
 import { store } from "@/modules/entity/actions";
 import EntityForm from "@/modules/entity/components/entity-form";
 import { Button, buttonVariants } from "@repo/ds/ui/button";
+import { useTranslations } from "next-intl";
 
 export default function Page() {
+	const t = useTranslations("entities");
+
 	return (
 		<main className="grow flex flex-col justify-center items-center my-16 px-2">
 			<h1 className="w-full max-w-5xl text-3xl font-bold mb-4 text-primary">
-				Registrar novo desaparecido
+				{t("create-title")}
 			</h1>
 
 			<EntityForm onSubmit={store} schema="store" require>
 				<Link href="/" className={buttonVariants({ variant: "outline" })}>
-					Voltar
+					{t("back")}
 				</Link>
 				<Button type="submit" className="w-full max-w-[180px]">
-					Criar
+					{t("create")}
 				</Button>
 			</EntityForm>
 		</main>
