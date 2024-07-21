091ba271f4260284977aa9f1947aeff2c0b7ddea
Rafael Correa <rafael.chaves98@hotmail.com>
2024-07-21 01:53:03 -0300

    Enhanced cicd workflow

--- apps/app/src/modules/user/loaders.ts
+++ apps/app/src/modules/user/loaders.ts
@@ -1,11 +1,14 @@
 import { auth } from "@clerk/nextjs/server";
 import userService from "@repo/services/user";
+import { headers } from "next/headers";
 import { cache } from "react";
 
 export const currentUser = cache(() => {
 	const clerkUser = auth().userId;
 
 	return clerkUser
-		? userService.byProvider({ provider: "clerk", value: clerkUser })({})
+		? userService.byProvider({ provider: "clerk", value: clerkUser })({
+				ip: headers().get("x-ip") as string,
+			})
 		: undefined;
 });
