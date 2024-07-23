9ed357426a214739c7398fae7ca7884c65168ba6
Rafael Corrêa Chaves <rafael.chaves98@hotmail.com>
2024-07-23 01:35:39 -0300

    [DEV-43] added more contact options types (#17)
    feat: added more contact options types

--- apps/app/src/app/[locale]/(app)/entities/[id]/_components/find-dialog.tsx
+++ apps/app/src/app/[locale]/(app)/entities/[id]/_components/find-dialog.tsx
@@ -7,6 +7,7 @@ import {
 	DialogDrawerTrigger,
 } from "@repo/ds/ui/dialog-drawer";
 import { Mail, Phone } from "lucide-react";
+import Image from "next/image";
 
 interface Props {
 	children: React.ReactNode;
@@ -41,22 +42,65 @@ export function FindDialog(props: Props) {
 					<ul className="flex flex-col gap-4">
 						{options
 							.filter((t) => t.value)
-							.map((option) => (
-								<li key={option.type + option.value}>
-									<a
-										href={`${option.type === "email" ? "mailto" : "tel"}:${option.value}`}
-										className={buttonVariants({
-											className: "flex justify-between w-full",
-										})}
-									>
-										{option.type === "email" ? <Mail /> : <Phone />}{" "}
-										{option.value}
-									</a>
-								</li>
-							))}
+							.map((option) => {
+								const info = getInfo(option.type, option.value);
+
+								if (!info) return null;
+
+								return (
+									<li key={option.type + option.value}>
+										<a
+											href={info.href}
+											className={buttonVariants({
+												className: "flex justify-between w-full",
+											})}
+										>
+											{info.icon} {info.render}
+										</a>
+									</li>
+								);
+							})}
 					</ul>
 				)}
 			</DialogDrawerContent>
 		</DialogDrawer>
 	);
+}
+
+function buildIcon(path: string) {
+	return (
+		<Image
+			src={`/images/brands/${path}.svg`}
+			width={16}
+			height={16}
+			alt={`${path} logo`}
+		/>
+	);
+}
+
+function getInfo(type: string, value: string) {
+	switch (type) {
+		case "phone":
+			return { href: `tel:${value}`, icon: <Phone />, render: value };
+		case "email":
+			return { href: `mailto:${value}`, icon: <Mail />, render: value };
+		case "whatsapp":
+			return {
+				href: `https://wa.me/${value.replace(/(\+|\-|\.|\(|\))/g, "")}`,
+				icon: buildIcon("whatsapp"),
+				render: value,
+			};
+		case "facebook":
+			return {
+				href: value.includes("http") ? value : `https://facebook.com/${value}`,
+				icon: buildIcon("facebook"),
+				render: value.split("facebook.com/").at(-1),
+			};
+		case "instagram":
+			return {
+				href: value.includes("http") ? value : `https://instagram.com/${value}`,
+				icon: buildIcon("instagram"),
+				render: value.split("instagram.com/").at(-1),
+			};
+	}
 }
