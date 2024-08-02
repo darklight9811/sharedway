f70a37762ab9b8bb9edbd9d038a7dbed88e43c23
Rafael Corrêa Chaves <rafael.chaves98@hotmail.com>
2024-08-01 23:08:12 -0300

    [DEV-53] added date_disappeared field (#25)
    feat: added date_disappeared field

--- packages/ds/src/components/ui/calendar.tsx
+++ packages/ds/src/components/ui/calendar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ptBR } from "date-fns/locale";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 
@@ -16,8 +17,9 @@ function Calendar({
 }: CalendarProps) {
 	return (
 		<DayPicker
+			locale={ptBR}
 			showOutsideDays={showOutsideDays}
-			className={cn("p-3", className)}
+			className={cn("pr-3", className)}
 			classNames={{
 				months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
 				month: "space-y-4",
