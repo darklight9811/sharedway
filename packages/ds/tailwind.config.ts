94503ce1d420c8bf5b9938319338c5a5495209ce
Rafael Correa <rafael.chaves98@hotmail.com>
2024-08-28 07:25:54 -0300

    chore: enhanced tests

--- packages/ds/tailwind.config.ts
+++ packages/ds/tailwind.config.ts
@@ -13,42 +13,23 @@ const config = {
 		},
 		extend: {
 			colors: {
-				border: "hsl(var(--border))",
-				input: "hsl(var(--input))",
-				ring: "hsl(var(--ring))",
-				background: "hsl(var(--background))",
-				foreground: "hsl(var(--foreground))",
+				border: "hsl(214.3 31.8% 91.4%)",
+				input: "hsl(214.3 31.8% 91.4%)",
+				ring: "hsl(222.2 84% 4.9%)",
+				background: "hsl(0 0% 100%)",
+				foreground: "hsl(0 2% 30%)",
 				primary: {
-					DEFAULT: "hsl(var(--primary))",
-					foreground: "hsl(var(--primary-foreground))",
+					DEFAULT: "hsl(208 87% 67%)",
+					foreground: "hsl(210 40% 98%)",
 				},
 				secondary: {
-					DEFAULT: "hsl(var(--secondary))",
-					foreground: "hsl(var(--secondary-foreground))",
+					DEFAULT: "hsl(198 38% 78%)",
+					foreground: "hsl(222.2 47.4% 11.2%)",
 				},
 				destructive: {
-					DEFAULT: "hsl(var(--destructive))",
-					foreground: "hsl(var(--destructive-foreground))",
+					DEFAULT: "hsl(0 84.2% 60.2%)",
+					foreground: "hsl(210 40% 98%)",
 				},
-				muted: {
-					DEFAULT: "hsl(var(--muted))",
-					foreground: "hsl(var(--muted-foreground))",
-				},
-				accent: {
-					DEFAULT: "hsl(var(--accent))",
-					foreground: "hsl(var(--accent-foreground))",
-				},
-				popover: {
-					DEFAULT: "hsl(var(--popover))",
-					foreground: "hsl(var(--popover-foreground))",
-				},
-				card: {
-					DEFAULT: "hsl(var(--card))",
-					foreground: "hsl(var(--card-foreground))",
-				},
-			},
-			fontSize: {
-				"clamp-title": "clamp(24px,3.75vw,48px)",
 			},
 			borderRadius: {
 				lg: "var(--radius)",
