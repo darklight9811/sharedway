99df4fdae3e8cdf5ea1fd3331134d9cbd859246e
Rafael Corrêa Chaves <rafael.chaves98@hotmail.com>
2024-09-02 23:26:39 -0300

    [DEV-12] Enhance profile creation page (#30)
    * chore: removed prisma ads
    
    * fix: 404 thrown on non localized pages
    
    * fix: missing textures on cards
    
    * feat: refactored profile form

--- apps/app/src/modules/profile/components/profile-form.tsx
+++ apps/app/src/modules/profile/components/profile-form.tsx
@@ -1,238 +1,67 @@
 "use client";
 
-import { states } from "@/modules/location/actions";
-import Field from "@repo/ds/form/field";
-import FieldList from "@repo/ds/form/field-list";
-import Form from "@repo/ds/form/form";
-import { AsyncSelect } from "@repo/ds/ui/async-select";
-import { Button } from "@repo/ds/ui/button";
-import { CalendarInput } from "@repo/ds/ui/calendar-input";
-import ImageUploader from "@repo/ds/ui/image-uploader";
-import { Input } from "@repo/ds/ui/input";
-import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
-} from "@repo/ds/ui/select";
-import { Textarea } from "@repo/ds/ui/textarea";
-import { profileStoreSchema, profileUpdateSchema } from "@repo/schemas/profile";
-import { Trash } from "lucide-react";
+import { Camera, MailIcon, MapIcon, User } from "lucide-react";
 import { useTranslations } from "next-intl";
+import { useState } from "react";
+
+import Form from "@repo/ds/form/form";
+import { Tabs, TabsList, TabsTrigger } from "@repo/ds/ui/tabs";
+import { profileStoreSchema } from "@repo/schemas/profile";
+
+import ProfileFormContact from "./profile-form.contact";
+import ProfileFormGeneral from "./profile-form.general";
+import ProfileFormLocation from "./profile-form.location";
+import ProfileFormPictures from "./profile-form.pictures";
 
 interface Props {
 	onSubmit(data: any): Promise<Record<string, unknown>>;
-	schema?: "store" | "update";
 	data?: Record<string, unknown>;
-	require?: boolean;
 	children?: React.ReactNode;
 }
 
-export default function ProfileForm(props: Props) {
+export default function ProfileFormV2(props: Props) {
 	const t = useTranslations("profiles.new");
+	const [page, setpage] = useState("general");
 
 	return (
 		<Form
 			data={props.data}
 			defaultData={{ contact: { options: [{}] }, date_disappeared: new Date() }}
-			schema={
-				props.schema === "update" ? profileUpdateSchema : profileStoreSchema
-			}
+			schema={profileStoreSchema}
 			onSubmit={props.onSubmit}
-			className="w-full max-w-5xl flex gap-8 flex-col"
+			className="w-full max-w-5xl flex gap-8 flex-col grow"
 		>
-			<div className="w-full flex flex-col md:flex-row gap-4 justify-between">
-				<div className="w-full sm:max-w-sm">
-					<h2 className="text-2xl mb-4">{t("general.title")}</h2>
-
-					<Field
-						label={t("general.name")}
-						name="name"
-						required={props.require}
-						render={({ field }) => {
-							return <Input {...field} />;
-						}}
-					/>
-
-					<Field
-						label={t("date_disappeared")}
-						name="date_disappeared"
-						required={props.require}
-						render={({ field }) => <CalendarInput {...field} />}
-					/>
-
-					<Field
-						label={t("general.age")}
-						name="data.age"
-						required={props.require}
-						render={({ field }) => {
-							return <Input {...field} />;
-						}}
-					/>
-
-					<Field
-						label={t("general.race")}
-						name="data.race"
-						required={props.require}
-						render={({ field }) => {
-							return <Input {...field} />;
-						}}
-					/>
-
-					<Field
-						label={t("general.gender")}
-						name="data.gender"
-						required={props.require}
-						render={({ field }) => {
-							return (
-								<Select {...field}>
-									<SelectTrigger>
-										<SelectValue />
-									</SelectTrigger>
-
-									<SelectContent>
-										<SelectItem value="male">
-											{t("general.gender-options.male")}
-										</SelectItem>
-										<SelectItem value="female">
-											{t("general.gender-options.female")}
-										</SelectItem>
-										<SelectItem value="other">
-											{t("general.gender-options.other")}
-										</SelectItem>
-									</SelectContent>
-								</Select>
-							);
-						}}
-					/>
-
-					<Field
-						label={t("general.description")}
-						name="description"
-						render={({ field }) => {
-							return <Textarea {...field} />;
-						}}
-					/>
+			<Tabs value={page} onValueChange={setpage} className="flex flex-col grow">
+				<div className="w-full flex justify-center sticky top-16">
+					<TabsList className="aspect-[5/1] flex">
+						<TabsTrigger value="general" className="flex flex-col gap-1 w-1/4">
+							<User />
+							<span className="text-xs opacity-50">{t("general.title")}</span>
+						</TabsTrigger>
+						<TabsTrigger value="pictures" className="flex flex-col gap-1 w-1/4">
+							<Camera />
+							<span className="text-xs opacity-50">{t("pictures.title")}</span>
+						</TabsTrigger>
+						<TabsTrigger value="location" className="flex flex-col gap-1 w-1/4">
+							<MapIcon />
+							<span className="text-xs opacity-50">{t("location.title")}</span>
+						</TabsTrigger>
+						<TabsTrigger value="contact" className="flex flex-col gap-1 w-1/4">
+							<MailIcon />
+							<span className="text-xs opacity-50">{t("contact.title")}</span>
+						</TabsTrigger>
+					</TabsList>
 				</div>
 
-				<div className="w-full max-w-[512px]">
-					<h2 className="text-2xl mb-4">{t("pictures.title")}</h2>
+				<ProfileFormGeneral />
+				<ProfileFormPictures />
+				<ProfileFormLocation />
+				<ProfileFormContact />
 
-					<Field
-						name="pictures"
-						render={({ field }) => {
-							return <ImageUploader max={5} {...field} />;
-						}}
-					/>
+				<div className="w-full flex gap-4 justify-center sticky bottom-0 bg-[#DBF0FD] p-4">
+					{props.children}
 				</div>
-			</div>
-
-			<div className="w-full flex flex-wrap gap-4 justify-between">
-				<h2 className="text-2xl mb-4 w-full">{t("location.title")}</h2>
-
-				<Field
-					label={t("location.district")}
-					required={props.require}
-					name="addresses.0.district"
-					className="w-full md:w-2/5"
-					render={({ field }) => {
-						return <Input {...field} />;
-					}}
-				/>
-
-				<Field
-					label={t("location.city")}
-					required={props.require}
-					name="addresses.0.city"
-					className="w-full md:w-2/5"
-					render={({ field }) => {
-						return <Input {...field} />;
-					}}
-				/>
-
-				<Field
-					label={t("location.state")}
-					required={props.require}
-					name="addresses.0.state"
-					className="w-full md:w-2/5"
-					render={({ field }) => {
-						return (
-							<AsyncSelect
-								{...field}
-								action={states}
-								t={{
-									empty: "Nenhum estado encontrado",
-									placeholder: "Escolha um estado",
-									loading: "Carregando",
-								}}
-							/>
-						);
-					}}
-				/>
-			</div>
-
-			<div className="w-full flex flex-wrap gap-4 justify-between">
-				<h2 className="text-2xl mb-4 w-full">{t("contact.title")}</h2>
-
-				<FieldList
-					name="contact.options"
-					startWith={1}
-					render={({ name, remove }) => (
-						<div className="flex gap-4 w-full" key={name}>
-							<Field
-								name={`${name}.type`}
-								value="phone"
-								className="m-0 min-w-[100px]"
-								render={({ field }) => (
-									<Select {...field}>
-										<SelectTrigger>
-											<SelectValue />
-										</SelectTrigger>
-
-										<SelectContent>
-											<SelectItem value="phone">
-												{t("contact.type-options.phone")}
-											</SelectItem>
-											<SelectItem value="email">
-												{t("contact.type-options.email")}
-											</SelectItem>
-											<SelectItem value="whatsapp">
-												{t("contact.type-options.whatsapp")}
-											</SelectItem>
-											<SelectItem value="facebook">
-												{t("contact.type-options.facebook")}
-											</SelectItem>
-											<SelectItem value="instagram">
-												{t("contact.type-options.instagram")}
-											</SelectItem>
-										</SelectContent>
-									</Select>
-								)}
-							/>
-							<Field
-								name={`${name}.value`}
-								className="m-0 w-full"
-								render={({ field }) => <Input {...field} />}
-							/>
-							<Button type="button" onClick={remove} variant="destructive">
-								<Trash />
-							</Button>
-						</div>
-					)}
-				/>
-
-				<Field
-					label={t("contact.description")}
-					name="contact.description"
-					className="w-full"
-					render={({ field }) => {
-						return <Textarea {...field} />;
-					}}
-				/>
-			</div>
-
-			<div className="mt-2 flex gap-2">{props.children}</div>
+			</Tabs>
 		</Form>
 	);
 }
--- packages/ds/tailwind.config.ts
+++ packages/ds/tailwind.config.ts
@@ -30,6 +30,22 @@ const config = {
 					DEFAULT: "hsl(0 84.2% 60.2%)",
 					foreground: "hsl(210 40% 98%)",
 				},
+				muted: {
+					DEFAULT: "hsl(210 40% 96.1%)",
+					foreground: "hsl(215.4 16.3% 46.9%)",
+				},
+				accent: {
+					DEFAULT: "hsl(210 40% 96.1%)",
+					foreground: "hsl(222.2 47.4% 11.2%)",
+				},
+				popover: {
+					DEFAULT: "hsl(0 0% 100%)",
+					foreground: "hsl(222.2 84% 4.9%)",
+				},
+				card: {
+					DEFAULT: "hsl(0 0% 100%)",
+					foreground: "hsl(222.2 84% 4.9%)",
+				},
 			},
 			borderRadius: {
 				lg: "var(--radius)",
